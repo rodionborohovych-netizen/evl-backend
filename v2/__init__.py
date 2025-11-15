9ebf94525ac784ca75d4d6fc5c8b8128a73e95f9
rodionborohovych-netizen <rodionborohovych@gmail.com>
2025-11-15 21:08:16 +0000

    Update __init__.py

--- v2/__init__.py
+++ v2/__init__.py
@@ -1,1 +1,10 @@
+"""
+EVL v2.0 - Business-Focused API Package
+
+Simplified location analysis with clear verdicts and recommendations.
+"""
+
+from .api_v2 import router_v2
+
+__all__ = ["router_v2"]
 
